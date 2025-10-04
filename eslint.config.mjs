3c22594a57662f85183e5248a7ac563de790a6af
Julia Nedavnia <nedavnaia@gmail.com>
2025-10-04 12:35:56 +0300

    Eslint configuration added

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import reactNativePlugin from "eslint-plugin-react-native";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: { ecmaFeatures: { jsx: true }, sourceType: "module" },
+      globals: { ...globals.browser, ...globals.node },
+    },
+
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      "react-native": reactNativePlugin,
+      "@typescript-eslint": tseslint.plugin,
+    },
+
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      reactPlugin.configs.flat.recommended
+    ],
+
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "react-native/no-inline-styles": "off",
+      "react-native/no-unused-styles": "warn",
+      "react-native/no-color-literals": "off",
+      "react-native/sort-styles": "off",
+      "react-native/split-platform-components": "warn",
+      "react-native/no-raw-text": "warn", 
+      "react-native/no-single-element-style-arrays": "warn",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+
+    settings: { react: { version: "detect" } },
+  },
+]);
+
