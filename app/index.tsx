e1f5f483ee21a1a77f2b20288a6d64b21d87451f
Julia Nedavnia <nedavnaia@gmail.com>
2025-11-01 12:18:00 +0200

    Route to other page

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 import React from 'react';
+import { Link, router } from 'expo-router';
 
 export default function HomePage() {
   return (
     <View>
       <Text>HomePage</Text>
+      <Link href="/user">
+        <Text>Go to user page</Text>
+      </Link>
+      <Pressable onPress={() => router.push('/user')}>
+        <Text>Go to user page 2</Text>
+      </Pressable>
     </View>
   );
 }
