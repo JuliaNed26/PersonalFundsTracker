62ff1a4ca78859e44b691ed783c26238450f077d
Julia Nedavnia <nedavnaia@gmail.com>
2025-11-01 12:35:22 +0200

    _layout.tsx added

--- app/profile/index.tsx
+++ app/profile/index.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 
-export default function userPage() {
+export default function profile() {
   return (
     <View>
-      <Text>userPage</Text>
+      <Text>profile</Text>
     </View>
   );
 }
